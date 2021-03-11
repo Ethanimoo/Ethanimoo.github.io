886cfcb2a39ea7dc768d28d4888d564639cda7dc
JoffreyLee <1027698430@qq.com>
2021-03-11 18:49:53 +0800

    推送到coding

--- deploy.sh
+++ deploy.sh
@@ -12,11 +12,11 @@ cd docs/.vuepress/dist
 git init
 git add -A
 git commit -m "deploy"
-git push -f https://github.com/JoffreyLee/JoffreyLee.github.io.git master:gh-pages # 推送到github
+git push -f git@github.com:JoffreyLee/JoffreyLee.github.io.git master:gh-pages # 推送到github
 
 cd - # 退回开始所在目录
 rm -rf docs/.vuepress/dist # 删除打包文件
 
 git add -A
 git commit -m "推送到coding"
-git push -f https://github.com/JoffreyLee/JoffreyLee.github.io.git master # 推送到coding
+git push -f git@github.com:JoffreyLee/JoffreyLee.github.io.git master # 推送到coding
